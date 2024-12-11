1d77f67ba82b5cd12595edf5369b6a6b1c7fe41e
Damilare Aleji <alejigideonabidemi@gmail.com>
2024-12-12 00:24:05 +0100

    First JS exercise

---
+++ script.js
@@ -0,0 +1,13 @@
+// select all buttons on the page
+
+const buttons = document.querySelectorAll('button');
+
+// add event listener to each button
+
+buttons.forEach(button => {
+  button.addEventListener('click', (event) => {
+    const stringToCompare = '';
+
+    
+})
+
